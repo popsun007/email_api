a2fa384698eddc3d809b1899a1ad64afb7eb80ed
Frank Sun <popsun2010@gmail.com>
2018-01-01 19:35:45 -0800

    Successfully sent a text email

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -1,16 +1,17 @@
-module API
+module Api
   module V1
     class EmailsController < ApplicationController
       def create
         @email = email_params
-        EmailMailer.eamil_body(@eamil).deliver
+
+        EmailMailer.send_email(@email).deliver
         render json: {status: 'SUCCESS', message: 'Sent email', data: @email}, status: :ok
       end
 
       private
 
         def email_params
-          params.permit(:to, :subject, :body)
+          params.permit(:to, :subject, :body, :email)
         end
     end
   end
--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,2 +1,6 @@
 class EmailMailer < ApplicationMailer
+  def send_email(email)
+    @email = email
+    mail(:to => email[:to], :subject => email[:subject])
+  end
 end
